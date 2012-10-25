42954fc960a0591441888d1f1d5c8f15592d473b
joe miller <joeym@joeym.net>
2012-10-25 07:11:26 -0700

    prompt is specific to mac osx boxes for now

--- _bash.d/prompt.bash
+++ _bash.d/prompt.bash
@@ -1,7 +1,5 @@
-################################################################################
-### This file installed by Chef. If you run bootstrap again, you'll lose
-### any changes made here.
-################################################################################
 
-# this will set the iterm2 tab name to 'local:<current directory>'
-PS1="$PS1\[$(echo -e '\033];LOCAL:$(basename $PWD)\007')\]"
+if [ "$(uname)" = "Darwin" ]; then
+    # this will set the iterm2 tab name to 'local:<current directory>'
+    PS1="$PS1\[$(echo -e '\033];LOCAL:$(basename $PWD)\007')\]"
+fi
