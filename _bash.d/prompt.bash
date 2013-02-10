edd498397cdda14b1776914ef3b9643549f72cae
joe miller <joeym@joeym.net>
2013-02-09 18:10:44 -0800

    moar better openbsd support

--- _bash.d/prompt.bash
+++ _bash.d/prompt.bash
@@ -1,5 +1,5 @@
 
-if [ "$(uname)" = "Darwin" ]; then
+if [ "$(uname)" = "Darwin" ] || [ "$(uname)" = "OpenBSD" ]; then
     # this will set the iterm2 tab name to 'local:<current directory>'
     PS1="$PS1\[$(echo -e '\033];LOCAL:$(basename $PWD)\007')\]"
 fi
