c5b7e95c4acfc0f9a00b37b168b23416f86ef341
joe miller <joeym@joeym.net>
2014-11-28 10:57:00 -0700

    add gcloud.bash for google cloud utils

---
+++ _bash.d/gcloud.bash
@@ -0,0 +1,7 @@
+# @TODO(joe): cross-platform support, $HOME
+
+# The next line updates PATH for the Google Cloud SDK.
+source '/Users/joe/google-cloud-sdk/path.bash.inc'
+
+# The next line enables bash completion for gcloud.
+source '/Users/joe/google-cloud-sdk/completion.bash.inc'
