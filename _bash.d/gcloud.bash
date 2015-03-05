309386cc85436908155f377ee5e4913f2234787a
joe miller <joeym@joeym.net>
2015-03-05 23:20:37 +0000

    don't source files that don't exist

--- _bash.d/gcloud.bash
+++ _bash.d/gcloud.bash
@@ -1,7 +1,8 @@
 # @TODO(joe): cross-platform support, $HOME
 
 # The next line updates PATH for the Google Cloud SDK.
-source '/Users/joe/google-cloud-sdk/path.bash.inc'
+[ -f "/Users/joe/google-cloud-sdk/path.bash.inc" ] && source "/Users/joe/google-cloud-sdk/path.bash.inc"
+
 
 # The next line enables bash completion for gcloud.
-source '/Users/joe/google-cloud-sdk/completion.bash.inc'
+[ -f "/Users/joe/google-cloud-sdk/completion.bash.inc" ] && source "/Users/joe/google-cloud-sdk/completion.bash.inc"
