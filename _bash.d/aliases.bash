1df01ab0e4ebb77626525e9e84f532e1c98fff4d
joe miller <joeym@joeym.net>
2015-05-18 19:03:40 -0700

    openbsd needs colorls installed, so detect that

--- _bash.d/aliases.bash
+++ _bash.d/aliases.bash
@@ -1,9 +1,22 @@
 case "$(uname)" in
-    "Darwin"|"OpenBSD")
+    "Darwin")
         alias l.='ls -G -d .*'
         alias ll='ls -G -l'
         alias lla='ls -G -la'
         alias la='ls -G -a'
+        ;;
+    "OpenBSD")
+        if [ -e "/usr/local/bin/colorls" ]; then
+          alias l.='/usr/local/bin/colorls -G -d .*'
+          alias ll='/usr/local/bin/colorls -G -l'
+          alias lla='/usr/local/bin/colorls -G -la'
+          alias la='/usr/local/bin/colorls -G -a'
+        else
+          alias l.='ls -d .*'
+          alias ll='ls -l'
+          alias lla='ls -la'
+          alias la='ls -a'
+        fi
         ;;
     "Linux")
         alias l.='ls -d .* --color=auto'
