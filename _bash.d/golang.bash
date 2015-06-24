c7feca1e9e916b38954144beab281db93ab51a12
joe miller <joeym@joeym.net>
2015-06-24 19:42:46 +0000

    suppress go warnings on nodes where go is not installed

--- _bash.d/golang.bash
+++ _bash.d/golang.bash
@@ -1,6 +1,8 @@
-unset GOBIN  # ensure $GOBIN is not set on onebox
-unset GOROOT
-unset GOPATH
-export GOPATH=$HOME/go
-export GOROOT=`go env GOROOT`
-export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+if which go >/dev/null 2>&1; then
+  unset GOBIN  # ensure $GOBIN is not set on onebox
+  unset GOROOT
+  unset GOPATH
+  export GOPATH=$HOME/go
+  export GOROOT=`go env GOROOT`
+  export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+fi
