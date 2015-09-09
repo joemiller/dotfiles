fd00e83932683615394caf38e57f2a8761908688
joe miller <joeym@joeym.net>
2015-09-09 06:57:45 -0700

    add gcloud zsh helpers

---
+++ _zsh.d/gcloud.zsh
@@ -0,0 +1,4 @@
+# @TODO(joe): cross-platform support, $HOME
+
+[ -f "/Users/joe/google-cloud-sdk/completion.zsh.inc" ] && source "/Users/joe/google-cloud-sdk/completion.zsh.inc"
+[ -f "/Users/joe/google-cloud-sdk/path.zsh.inc" ] && source "/Users/joe/google-cloud-sdk/path.zsh.inc"
