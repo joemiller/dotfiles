ae251807ba7da681f817580464825e5003046791
joe miller <joeym@joeym.net>
2015-09-28 20:37:11 -0700

    add more ruby/gem config

---
+++ _bash.d/ruby.zsh
@@ -0,0 +1,1 @@
+_zsh.d/ruby.zsh
--- _zsh.d/ruby.zsh
+++ _zsh.d/ruby.zsh
@@ -1,3 +1,5 @@
 [ -d "$HOME/.gem/ruby/2.0.0/bin" ] && export PATH=$PATH:"$HOME/.gem/ruby/2.0.0/bin"
+export GEM_HOME="$HOME/.gem"
 
 alias be='bundle exec'
+alias bi='bundle install'
