bfd5493055f73d87950a764c5002145f6b1694b2
joe miller <joeym@joeym.net>
2015-10-22 13:32:20 -0700

    fix variable name

--- _zsh.d/packer.zsh
+++ _zsh.d/packer.zsh
@@ -1,34 +1,34 @@
 case "$OSTYPE" in
   darwin*)
     if [ -d "$HOME/Dropbox/packer-osx" ]; then
-      docker_path="$HOME/Dropbox/packer-osx"
+      packer_path="$HOME/Dropbox/packer-osx"
     elif [ -d "$HOME/packer-osx" ]; then
-      docker_path="$HOME/packer-osx"
+      packer_path="$HOME/packer-osx"
     fi
     ;;
 
   linux*)
     if [ -d "$HOME/Dropbox/packer-linux" ]; then
-      docker_path="$HOME/Dropbox/packer-linux"
+      packer_path="$HOME/Dropbox/packer-linux"
     elif [ -d "$HOME/packer-linux" ]; then
-      docker_path="$HOME/packer-linux"
+      packer_path="$HOME/packer-linux"
     fi
     ;;
 
   openbsd*)
     if [ -d "$HOME/Dropbox/packer-openbsd" ]; then
-      docker_path="$HOME/Dropbox/packer-openbsd"
+      packer_path="$HOME/Dropbox/packer-openbsd"
     elif [ -d "$HOME/packer-openbsd" ]; then
-      docker_path="$HOME/packer-openbsd"
+      packer_path="$HOME/packer-openbsd"
     fi
     ;;
   freebsd*)
     if [ -d "$HOME/Dropbox/packer-freebsd" ]; then
-      docker_path="$HOME/Dropbox/packer-freebsd"
+      packer_path="$HOME/Dropbox/packer-freebsd"
     elif [ -d "$HOME/packer-freebsd" ]; then
-      docker_path="$HOME/packer-freebsd"
+      packer_path="$HOME/packer-freebsd"
     fi
     ;;
 esac
 
-export PATH="$docker_path:$PATH"
+export PATH="$packer_path:$PATH"
