e706aebb06e784e9b39b3dc5c8640853aadbf020
joe miller <joeym@joeym.net>
2016-02-08 17:35:36 -0800

    only run gpg-agent on local workstation

--- _bash.d/gpg-agent.sh
+++ _bash.d/gpg-agent.sh
@@ -1,3 +1,9 @@
-[ -e "${HOME}/.gpg-agent-info" ] && source "${HOME}/.gpg-agent-info"
-gpg-connect-agent /bye &>/dev/null || gpg-agent --daemon &>/dev/null
-source "${HOME}/.gpg-agent-info"
+# XXX: sort of a hack... We only want to run gpg-agent on local workstation. For now, we can probably
+#      do this by only running the agent on mac osx, though this is not perfect.
+if [[ "$OSTYPE" =~ darwin ]]; then
+	[ -e "${HOME}/.gpg-agent-info" ] && source "${HOME}/.gpg-agent-info"
+	gpg-connect-agent /bye &>/dev/null || gpg-agent --daemon &>/dev/null
+	source "${HOME}/.gpg-agent-info"
+	GPG_TTY=$(tty)
+	export GPG_TTY
+fi
