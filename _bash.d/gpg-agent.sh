0c1ee2ec01aaab5c61a6b344890cca900dd05c14
joe miller <joeym@joeym.net>
2016-02-09 08:03:00 -0800

    use gpgconf with gpg >= 2.1

--- _bash.d/gpg-agent.sh
+++ _bash.d/gpg-agent.sh
@@ -3,9 +3,15 @@
 # this is probably a remote login and we don't want to run gpg-agent.
 
 if [ ! -n "$SSH_CLIENT" ]; then
-	[ -e "${HOME}/.gpg-agent-info" ] && source "${HOME}/.gpg-agent-info"
-	gpg-connect-agent /bye &>/dev/null || gpg-agent --daemon &>/dev/null
-	source "${HOME}/.gpg-agent-info"
+  # gpg < 2.1, use gpg-connect-agent
+  #[ -e "${HOME}/.gpg-agent-info" ] && source "${HOME}/.gpg-agent-info"
+  #gpg-connect-agent /bye &>/dev/null || gpg-agent --daemon &>/dev/null
+	#source "${HOME}/.gpg-agent-info"
+
+  # gpg >= 2.1, use gpgconf
+  gpgconf --launch gpg-agent
+  export SSH_AUTH_SOCK="${HOME}/.gnupg/S.gpg-agent.ssh"
+
 	GPG_TTY=$(tty)
 	export GPG_TTY
 fi
