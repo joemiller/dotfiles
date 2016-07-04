dfb4a64b752a1c14c3610ed1148fd592a2517322
joe miller <joeym@joeym.net>
2016-07-04 09:20:44 -0700

    update docker-* helpers to cover new docker-machine and docker.app co-existing

--- _common.sh.d/docker.sh
+++ _common.sh.d/docker.sh
@@ -1,7 +1,13 @@
 # https://docs.docker.com/installation/mac/
 
-function docker-env() {
-  eval "$(docker-machine env default)"
+docker-env() {
+  machine="$1"
+  eval "$(docker-machine env $machine)"
+}
+
+# unset docker env vars (use this to switch to native docker.app for example)
+docker-env-unset() {
+  unset $(env | awk -F= '/DOCKER_/ {print $1}')
 }
 
 alias docker-cleanup-images='docker images -q --filter "dangling=true" | xargs -P8 docker rmi'
