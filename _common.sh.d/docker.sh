b4afacefa1e6bead1833ec1022f7c13da825e838
joe miller <joeym@joeym.net>
2017-09-23 16:51:36 -0700

    add helper tool docker-install-ubuntu

--- _common.sh.d/docker.sh
+++ _common.sh.d/docker.sh
@@ -14,3 +14,26 @@ alias docker-cleanup-images='docker images -q --filter "dangling=true" | xargs -
 alias docker-cleanup-containers='docker rm $(docker ps -a -q)'
 
 alias dm='docker-machine'
+
+# docker-install-ubuntu is a helper for installing the docker apt repos and the docker-ce deb on an ubuntu node.
+# it's based on docker's official instructions as of 09/2017.
+docker-install-ubuntu() {
+  # allow overriding the inferred ubuntu release name
+  local UBUNTU_RELEASE=${UBUNTU_RELEASE:-$(lsb_release -cs)}
+  sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    "$UBUNTU_RELEASE" \
+    stable edge"
+  sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    "$UBUNTU_RELEASE" \
+    edge"
+  sudo apt-get update
+  sudo apt-get install docker-ce
+}
