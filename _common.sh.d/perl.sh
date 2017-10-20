60aa91b9b7437d3540f68d6ba10ae7a83754c75b
joe miller <joeym@joeym.net>
2017-10-20 06:29:18 -0700

    add bash and zsh code to support perl local::lib

---
+++ _common.sh.d/perl.sh
@@ -0,0 +1,13 @@
+# support for perl local::lib if installed
+# to setup perl local::lib and cpannm command:
+#    $ cpan App::cpanminus
+#
+# http://www.cpan.org/modules/INSTALL.html
+
+if [[ -e "$HOME/perl5/bin" ]]; then
+    PATH="$HOME/perl5/bin${PATH:+:${PATH}}"; export PATH;
+    PERL5LIB="$HOME/perl5/lib/perl5${PERL5LIB:+:${PERL5LIB}}"; export PERL5LIB;
+    PERL_LOCAL_LIB_ROOT="$HOME/perl5${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}"; export PERL_LOCAL_LIB_ROOT;
+    PERL_MB_OPT="--install_base \"$HOME/perl5\""; export PERL_MB_OPT;
+    PERL_MM_OPT="INSTALL_BASE=$HOME/perl5"; export PERL_MM_OPT;
+fi
