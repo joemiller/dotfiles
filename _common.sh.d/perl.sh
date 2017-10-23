9819e3f1251aeda131677404eb8c75a74d93a287
joe miller <joeym@joeym.net>
2017-10-23 14:45:33 -0700

    move cpanm dir from $HOME/perl5 to $HOME/.perl5 to reduce home dir clutter

--- _common.sh.d/perl.sh
+++ _common.sh.d/perl.sh
@@ -4,10 +4,14 @@
 #
 # http://www.cpan.org/modules/INSTALL.html
 
-if [[ -e "$HOME/perl5/bin" ]]; then
-    PATH="$HOME/perl5/bin${PATH:+:${PATH}}"; export PATH;
-    PERL5LIB="$HOME/perl5/lib/perl5${PERL5LIB:+:${PERL5LIB}}"; export PERL5LIB;
-    PERL_LOCAL_LIB_ROOT="$HOME/perl5${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}"; export PERL_LOCAL_LIB_ROOT;
-    PERL_MB_OPT="--install_base \"$HOME/perl5\""; export PERL_MB_OPT;
-    PERL_MM_OPT="INSTALL_BASE=$HOME/perl5"; export PERL_MM_OPT;
+# NOTE! cpanm will default to "$HOME/perl5" but we use "$HOME/.perl5" instead!!
+
+CPANM_BASEDIR="$HOME/.perl5"
+
+if [[ -e "$CPANM_BASEDIR/bin" ]]; then
+    PATH="$CPANM_BASEDIR/bin${PATH:+:${PATH}}"; export PATH;
+    PERL5LIB="$CPANM_BASEDIR/lib/perl5${PERL5LIB:+:${PERL5LIB}}"; export PERL5LIB;
+    PERL_LOCAL_LIB_ROOT="$CPANM_BASEDIR${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}"; export PERL_LOCAL_LIB_ROOT;
+    PERL_MB_OPT="--install_base \"$CPANM_BASEDIR\""; export PERL_MB_OPT;
+    PERL_MM_OPT="INSTALL_BASE=$CPANM_BASEDIR"; export PERL_MM_OPT;
 fi
