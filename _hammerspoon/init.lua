4eae33d303ddc6673eeb95dec0015b5be1530c2e
joe miller <joeym@joeym.net>
2019-01-25 11:23:46 -0800

    renable wifi_watcher because the auto-mute is nice, but disable auto-joining vpns

--- _hammerspoon/init.lua
+++ _hammerspoon/init.lua
@@ -13,24 +13,25 @@ require "anycomplete"
 hs.window.animationDuration = 0
 
 -- recognize our home networks so we can take action on connection to "foreign" networks
-wifi.homeSSIDs = {"reddragon-5G", "reddragon-2G"}
+-- wifi.homeSSIDs = {"reddragon-5G", "reddragon-2G"}
 
 -- auto-connect to Tunnelblick VPNs when connected to these networks
-wifi.autoConnectVPNs = {
-    ["BREW BROS"] = "home-joeym-net (all traffic)",
-    ["AnotherGuest"] = "home-joeym-net (all traffic)",
-    ["#SFWiFi"] = "home-joeym-net (all traffic)",
-    ["the brew"] = "home-joeym-net (all traffic)",
-    ["LIQUIDGOLD_PUBLIC"] = "home-joeym-net (all traffic)",
-    ["Pantheon"] = "home-joeym-net",
-    ["Pantheon-Secure"] = "home-joeym-net",
-    ["Pantheon Guest"] = "home-joeym-net",
-    ["Pantheon-5G"] = "home-joeym-net",
-    ["Pantheon-5G"] = "home-joeym-net",
-    ["Pantheon-5G-802.11r"] = "home-joeym-net",
-    ["jwm"] = "home-joeym-net",
-    ["jwm-n"] = "home-joeym-net",
-}
+-- wifi.autoConnectVPNs = {
+--     ["BREW BROS"] = "home-joeym-net (all traffic)",
+--     ["AnotherGuest"] = "home-joeym-net (all traffic)",
+--     ["#SFWiFi"] = "home-joeym-net (all traffic)",
+--     ["the brew"] = "home-joeym-net (all traffic)",
+--     ["LIQUIDGOLD_PUBLIC"] = "home-joeym-net (all traffic)",
+--     ["Pantheon"] = "home-joeym-net",
+--     ["Pantheon-Secure"] = "home-joeym-net",
+--     ["Pantheon Guest"] = "home-joeym-net",
+--     ["Pantheon-5G"] = "home-joeym-net",
+--     ["Pantheon-5G"] = "home-joeym-net",
+--     ["Pantheon-5G-802.11r"] = "home-joeym-net",
+--     ["jwm"] = "home-joeym-net",
+--     ["jwm-n"] = "home-joeym-net",
+-- }
+wifi.autoConnectVPNs = {}
 
 -- global key binds
 hs.hotkey.bind({"ctrl", "alt",      }, "F",     winmod.toggleMaximized)
