e5706e3c04a1489f677e5ca2425f3c3e55c4a4e7
joe miller <joeym@joeym.net>
2019-07-27 16:32:14 -0700

    common.sh.d/kube.sh: do not try to load kubectl completions if kubectl is not installed

--- dot_common.sh.d/kube.sh
+++ dot_common.sh.d/kube.sh
@@ -8,13 +8,15 @@ alias kns='kube-ns'
 
 alias kube-whereami='kubectl config current-context'
 
-# load shell completions
-if [[ -n "$ZSH_VERSION" ]]; then
-    source <(kubectl completion zsh)
-    # a helper alias because this seems to constantly be breaking when loaded from .zshrc
-    alias kube-completions='source <(kubectl completion zsh)'
-fi
+if command -v kubectl >/dev/null; then
+  # load shell completions
+  if [[ -n "$ZSH_VERSION" ]]; then
+      source <(kubectl completion zsh)
+      # a helper alias because this seems to constantly be breaking when loaded from .zshrc
+      alias kube-completions='source <(kubectl completion zsh)'
+  fi
 
-if [[ -n "$BASH_VERSION" ]]; then
-    source <(kubectl completion bash)
+  if [[ -n "$BASH_VERSION" ]]; then
+      source <(kubectl completion bash)
+  fi
 fi
