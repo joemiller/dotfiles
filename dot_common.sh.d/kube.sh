bb7c87eab39fbc8038d38e3d4b99e81620e35d6e
joe miller <joeym@joeym.net>
2019-08-26 08:15:18 -0700

    common.sh.d/kube.sh: add kubeconfig-cleanup alias

--- dot_common.sh.d/kube.sh
+++ dot_common.sh.d/kube.sh
@@ -24,3 +24,7 @@ fi
 if [[ -d "$HOME/.krew/bin" ]]; then
   export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
 fi
+
+# kubectl krew install config-cleanup
+# https://github.com/b23llc/kubectl-config-cleanup
+alias kubeconfig-cleanup='cp ~/.kube/config ~/.kube/config.bak && kubectl config-cleanup --raw -t10 > ~/.kube/config-cleaned && mv ~/.kube/config-cleaned ~/.kube/config'
