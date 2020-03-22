c6470195989186eba0001ea0a985515fb6b331cc
joe miller <joeym@joeym.net>
2020-03-22 13:31:54 -0700

    implement tools command
    Added new command `tools`. The purpose of this tool is to consolidate
    all helper scripts under a single umbrella command so they're easier to
    find.
    
    The typical usage is `tools <subcommand>` where <subcommand> is an
    executable in `$PATH` that follows the naming convention
    `tools-<subcommand>`. Inspired by git and kubectl plugins.
    
    Usage:
    
    Run `tools -h` to list available subcommands.
    
    Completion support for bash and zsh are implemented as well. Run `tools
    -h` for instructions.

--- dot_zsh.d/antigen-helpers.zsh
+++
@@ -1,5 +0,0 @@
-# use this because 'antigen selfupdate' does not work in this configuration
-# https://github.com/zsh-users/antigen/wiki/Installation
-tools-antigen-selfupdate() {
-  curl -L git.io/antigen > "$HOME/.antigen.zsh"
-}
