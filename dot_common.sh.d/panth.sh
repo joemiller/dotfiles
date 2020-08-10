9509299c063245e967b500e64f174479ab90532e
joe miller <joeym@joeym.net>
2020-08-10 10:10:54 -0700

    update psh

--- dot_common.sh.d/panth.sh
+++ dot_common.sh.d/panth.sh
@@ -8,14 +8,7 @@ jenkins() {
 }
 alias jenk=jenkins
 
-psh() {
-  host=$1
-  if [ -z "$host" ]; then
-    echo "usage: psh <short-hostname>"
-    return 1
-  fi
-  ssh "$host.panth.io"
-}
+alias psh='pants ssh'
 
 alias kube-panth-internal='kubectl config use-context gke_pantheon-internal_us-central1-b_us-central1-b-cluster-1'
 
