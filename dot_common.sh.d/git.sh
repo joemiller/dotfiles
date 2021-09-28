68939c80e90f84d0a0eabdbb190d1ab48dd8e105
joe miller <joeym@joeym.net>
2021-09-28 07:11:12 -0700

    git: add new "gr" alias (git root)

--- dot_common.sh.d/git.sh
+++ dot_common.sh.d/git.sh
@@ -1,5 +1,9 @@
 alias g=git
 
+# TODO: update pr to use the new github cli
 alias pr='hub pull-request'
 alias gup-master='gco master && gup'
 alias gup-main='gco main && gup'
+
+# 'git root': cd back to root of the current repo. from: https://bezhermoso.github.io/2018/07/28/jump-back-up-to-git-repo-root-directory/
+alias gr='cd $(git rev-parse --show-toplevel)'
