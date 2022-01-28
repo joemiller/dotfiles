6e494b8064b8894c17b0f34cbd70b104848cd4b9
joe miller <joeym@joeym.net>
2022-01-28 11:04:54 -0800

    git: add groot alias to cd back to git repo root

--- dot_common.sh.d/git.sh
+++ dot_common.sh.d/git.sh
@@ -6,4 +6,4 @@ alias gup-master='gco master && gup'
 alias gup-main='gco main && gup'
 
 # 'git root': cd back to root of the current repo. from: https://bezhermoso.github.io/2018/07/28/jump-back-up-to-git-repo-root-directory/
-alias gr='cd $(git rev-parse --show-toplevel)'
+alias groot='cd $(git rev-parse --show-toplevel)'
