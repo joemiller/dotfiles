19424336283a501c7b33a6ce0bf67ad9b8e1b91e
joe miller <joeym@joeym.net>
2022-04-06 14:07:02 -0700

    gcloud.sh: no longer need to set CLOUDSDK_PYTHON on darwin

--- dot_common.sh.d/gcloud.sh
+++ dot_common.sh.d/gcloud.sh
@@ -1,9 +1,3 @@
-# on macOS, force gcloud to use /usr/bin/python which is the python2.7 that gcloud requires.
-# This is necessary since homebrew switched its default python recipe to python3
-if [ "$(uname)" = "Darwin" ]; then
-    export CLOUDSDK_PYTHON=/usr/bin/python
-fi
-
 if [[ -n "$ZSH_VERSION" ]]; then
     [[ -f "$HOME/google-cloud-sdk/completion.zsh.inc" ]] && source "$HOME/google-cloud-sdk/completion.zsh.inc"
     [[ -f "$HOME/google-cloud-sdk/path.zsh.inc" ]] && source "$HOME/google-cloud-sdk/path.zsh.inc"
