ac419429aea0023d147a5466c96ed65d888f6809
joe miller <joeym@joeym.net>
2023-03-25 09:22:05 -0700

    common.sh.d: add the_platinum_searcher.sh

---
+++ dot_common.sh.d/the_platinum_searcher.sh
@@ -0,0 +1,12 @@
+# aliases for the platinum searcher (pt) https://github.com/monochromegane/the_platinum_searcher
+alias pt-tf="pt -G '\.tf'"
+alias pt-hcl="pt -G '\.tf'"
+alias pt-go="pt -G '\.go'"
+alias pt-sh="pt -G '\.sh'"
+alias pt-py="pt -G '\.py'"
+alias pt-rb="pt -G '\.rb'"
+alias pt-yaml="pt -G '\.yaml'"
+alias pt-json="pt -G '\.json'"
+
+# let pt recurse into '.*' dirs
+alias pt-hidden="pt --hidden"
