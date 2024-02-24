62a7f1b959c786ff12aa231a003e855522b559b0
joe miller <joeym@joeym.net>
2024-02-24 11:16:23 -0800

    hammerspoon: app jump menu updates

--- dot_hammerspoon/app_jump_menu.lua
+++ dot_hammerspoon/app_jump_menu.lua
@@ -18,14 +18,13 @@ string.format(
     d - VSCode \
     e - evernote \
     f - finder \
-    i - iterm \
     k - kitty \
     l - slack \
     s - spotify \
     m - iMessage \
+    o - Obsidian \
     t - thinkorswim \
     n - Notification Center \
-    v - vivaldi (browser) \
     z - zoom \
     . - Do not disturb (toggle) \
     / - hammerspoon console \
@@ -46,18 +45,21 @@ modal:bind('','return', function() modal:exit() end)
 
 -- hotkeys while modal is active
 modal:bind('','a', function() hs.application.launchOrFocus( "Alacritty" )      ; modal:exit() end )
-modal:bind('','v', function() hs.application.launchOrFocus( "Vivaldi" )        ; modal:exit() end )
+-- modal:bind('','v', function() hs.application.launchOrFocus( "Vivaldi" )        ; modal:exit() end )
 modal:bind('','c', function() hs.application.launchOrFocus( "Google Chrome" )  ; modal:exit() end )
 modal:bind('','d', function() hs.application.launchOrFocus( "Visual Studio Code" )  ; modal:exit() end )
 modal:bind('','e', function() hs.application.launchOrFocus( "Evernote.app" )   ; modal:exit() end )
-modal:bind('','i', function() hs.application.launchOrFocus( "iTerm.app" )      ; modal:exit() end )
+-- modal:bind('','i', function() hs.application.launchOrFocus( "iTerm.app" )      ; modal:exit() end )
 modal:bind('','k', function() hs.application.launchOrFocus( "kitty.app" )      ; modal:exit() end )
+modal:bind('','o', function() hs.application.launchOrFocus( "Obsidian.app" )   ; modal:exit() end )
 modal:bind('','f', function() hs.application.launchOrFocus( "Finder.app" )     ; modal:exit() end )
+modal:bind('','s', function() hs.application.launchOrFocus( "Spotify.app" )    ; modal:exit() end )
 modal:bind('','s', function() hs.application.launchOrFocus( "Spotify.app" )    ; modal:exit() end )
 modal:bind('','l', function() hs.application.launchOrFocus( "Slack.app" )      ; modal:exit() end )
 modal:bind('','m', function() hs.application.launchOrFocus( "Messages.app" )   ; modal:exit() end )
-modal:bind('','t', function() hs.application.launchOrFocus( "thinkorswim.app" )   ; modal:exit() end )
+modal:bind('','t', function() hs.application.launchOrFocus( "thinkorswim.app" ); modal:exit() end )
+-- modal:bind('','u', function() hs.application.launchOrFocus( "UpNote.app" )     ; modal:exit() end )
 modal:bind('','n', function() nc.showNotificationCenter()                      ; modal:exit() end )
 modal:bind('','.', function() nc.toggleDoNotDisturb()                          ; modal:exit() end )
 modal:bind('','/', function() hs.toggleConsole()                               ; modal:exit() end )
-modal:bind('','z', function() hs.application.launchOrFocus( "zoom.us.app" )   ; modal:exit() end )
+modal:bind('','z', function() hs.application.launchOrFocus( "zoom.us.app" )    ; modal:exit() end )
