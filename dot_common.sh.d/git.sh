758da0073fa82b15e5cfc7525dc3bc3bc4f1dc4f
joe miller <joeym@joeym.net>
2024-05-10 01:11:47 +0000

    common.sh.d/git: add "vci" alias

--- dot_common.sh.d/git.sh
+++ dot_common.sh.d/git.sh
@@ -1,9 +1,12 @@
 alias g=git
 
-# TODO: update pr to use the new github cli
+g TODO: update pr to use the new github cli
 alias pr='hub pull-request'
 alias gup-master='gco master && gup'
 alias gup-main='gco main && gup'
 
 # 'git root': cd back to root of the current repo. from: https://bezhermoso.github.io/2018/07/28/jump-back-up-to-git-repo-root-directory/
 alias groot='cd $(git rev-parse --show-toplevel)'
+
+# vci to use vscode for git commit message authoring
+alias vci='EDITOR="code --wait" git ci'
