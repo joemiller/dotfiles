3abebe97572be4dc11bedd23caf31e2ea5291882
joe miller <joeym@joeym.net>
2024-12-09 20:59:57 +0000

    ps: psk-name2psid

--- dot_common.sh.d/pscale.sh
+++ dot_common.sh.d/pscale.sh
@@ -1,3 +1,29 @@
 [ -d "$HOME/.ps-toolbox/bin" ] && export PATH="$HOME/.ps-toolbox/bin:$PATH"
 
-#alias psk=pskube
+alias pk=pskube
+alias psk=pskube
+
+psk-psid2name() {
+  local _psid="$1"
+  pskube --list-json | jq --arg psid "$_psid" -r '.[] | select(.psID == $psid) | .name'
+}
+
+psk-psid2alias() {
+  local _psid="$1"
+  pskube --list-json | jq --arg psid "$_psid" -r '.[] | select(.psID == $psid) | .alias'
+}
+
+psk-alias2psid() {
+  local _alias="$1"
+  pskube --list-json | jq --arg alias "$_alias" -r '.[] | select(.alias == $alias) | .psID'
+}
+
+psk-alias2name() {
+  local _alias="$1"
+  pskube --list-json | jq --arg alias "$_alias" -r '.[] | select(.alias == $alias) | .name'
+}
+
+psk-name2psid() {
+  local _name="$1"
+  pskube --list-json | jq --arg name "$_name" -r '.[] | select(.name == $name) | .psID'
+}
