7211c883d8ca6afe4c0878f2ac27efa9e290db51
joe miller <joeym@joeym.net>
2025-06-16 15:40:45 +0000

    add docker-list-images alias

--- dot_common.sh.d/docker.sh
+++ dot_common.sh.d/docker.sh
@@ -1,1 +1,4 @@
 alias dcc=docker-compose
+
+# list docker images sorted by size, smallest to largest
+alias docker-list-images='docker image ls --format "{{.Size}} {{.ID}} {{.Repository}}:{{.Tag}}" | LANG=en_US sort -h | column -t'
